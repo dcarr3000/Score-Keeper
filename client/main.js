b7ef8d1f695d523fa92dfefda9ec2ed2fe4ba2b3
David Carron <dcarr3000@gmail.com>
2018-04-26 13:30:41 -0500

    First commit

---
+++ client/main.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Meteor} from 'meteor/meteor';
+import {Players} from './../imports/api/players.js';
+import {Tracker} from 'meteor/tracker';
+import TitleBar from './../imports/ui/titlebar.js';
+import AddPlayer from './../imports/ui/addplayer.js';
+
+
+const renderPlayers = (playersList) => {
+	
+	
+	return playersList.map((player) => {
+		return( 
+		<p key={player._id}>
+		{player.name} has {player.score} point(s).
+		<button onClick={() => { Players.update(player._id, {$inc: {score: 1}});
+		}}>+1</button>
+		<button onClick={() => { Players.update(player._id, {$inc: {score: -1}});
+		}}>-1</button>
+		<button onClick={() => Players.remove(player._id)}>X</button>
+		</p>
+	);
+		
+	});
+};
+
+
+
+
+Meteor.startup(() => {
+	
+	Tracker.autorun(() => {
+	let players = Players.find().fetch();
+
+	let title = 'Bach Bowl';
+	let subtitle = 'Created by David Carron';
+	let jsx = 	(
+		<div>
+			<TitleBar title={title} subtitle={subtitle}/>
+			{renderPlayers(players)}
+			<AddPlayer/>
+			
+		</div>
+		);
+	ReactDOM.render(jsx, document.getElementById('app'));
+	
+	
+	});
+	
+	
+	
+	
+});
